5612d9f6a75b121d0ec7149dc246b0d9cd80f596
Seamus93 <simonefioroni93@gmail.com>
2024-09-18 23:29:25 +0200

    Update AISecurity.sol

--- contracts/AISecurity.sol
+++ contracts/AISecurity.sol
@@ -1,29 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract AISecurity {
-    struct ModelInfo {
-        string modelName;
-        string version;
-        bool isValid;
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "./Verifier.sol";
+
+contract AISecurity is AutomationCompatibleInterface, ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    Verifier public zkVerifier;
+    bytes32 public aiExpectedHash;
+    string private jobId;
+    uint256 private fee;
+    bytes32 public aiHash;
+
+    constructor(
+        address _verifierAddress,
+        bytes32 _aiExpectedHash,
+        address _linkToken,
+        address _oracle
+    ) ConfirmedOwner(msg.sender) {
+        zkVerifier = Verifier(_verifierAddress);
+        aiExpectedHash = _aiExpectedHash;
+        setChainlinkToken(_linkToken);
+        setChainlinkOracle(_oracle);
+        jobId = "YOUR_JOB_ID"; // Inserisci l'ID del tuo lavoro Chainlink Functions
+        fee = 0.1 * 10 ** 18; // Inserisci il fee in LINK
     }
 
-    mapping(string => ModelInfo) public models;
-
-    function registerModel(string memory modelName, string memory version) public {
-        models[modelName] = ModelInfo({
-            modelName: modelName,
-            version: version,
-            isValid: false
-        });
+    function verifyContractUpgrade(bytes memory proof) public view returns (bool) {
+        (bool success, ) = zkVerifier.verifyTx(proof, new uint256 );
+        return success;
     }
 
-    function validateModel(string memory modelName) public {
-        require(bytes(models[modelName].modelName).length != 0, "Model does not exist");
-        models[modelName].isValid = true;
+    function validateAIIntegrity() public view returns (bool) {
+        return aiHash == aiExpectedHash;
     }
 
-    function isModelValid(string memory modelName) public view returns (bool) {
-        return models[modelName].isValid;
+    function requestAIHash() public onlyOwner {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillAIHash.selector
+        );
+        sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfillAIHash(bytes32 _aiHash) public recordChainlinkFulfillment(requestId) {
+        aiHash = _aiHash;
+    }
+
+    function checkUpkeep(bytes calldata) external view override returns (bool upkeepNeeded, bytes memory) {
+        upkeepNeeded = !validateAIIntegrity();
+        return (upkeepNeeded, "");
+    }
+
+    function performUpkeep(bytes calldata) external override {
+        // Implementa la logica di aggiornamento o controllo periodico qui
+        if (!validateAIIntegrity()) {
+            // Azioni da intraprendere se l'integrità dell'AI non è valida
+        }
     }
 }
