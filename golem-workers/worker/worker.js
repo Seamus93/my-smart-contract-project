87aa5977808d194bd47fc70efd2a39b8efdb212e
Seamus93 <simonefioroni93@gmail.com>
2025-01-31 17:14:33 +0000

    prova aggiornata

--- golem-workers/worker/worker.js
+++ golem-workers/worker/worker.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 const dotenv = require('dotenv');
 const winston = require('winston');
 
